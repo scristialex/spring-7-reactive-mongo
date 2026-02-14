18d50ffa12622b398fea6aed8fa86c1e6d18d735
Cristi <scristialex@gmail.com>
2026-02-14 17:40:32 +0200

    Initial configuration

---
+++ src/main/java/guru/springframework/reactivemongo/Spring7ReactiveMongoApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.reactivemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring7ReactiveMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring7ReactiveMongoApplication.class, args);
+    }
+
+}
